81c26cd9136008f19135b2c4bfb8e400ae0efff3
derdon <liedtke.simon@googlemail.com>
2012-02-26 23:41:16 +0100

    implemented basedir spec

---
+++ basedir/envvars.go
@@ -0,0 +1,22 @@
+package basedir
+
+import "os"
+
+var home = os.Getenv("$HOME")
+var local = home + "/.local"
+var (
+	XDG_DATA_HOME = getenv("XDG_DATA_HOME", local + "/share")
+	XDG_CONFIG_HOME = getenv("XDG_CONFIG_HOME", home + "/.config")
+	XDG_DATA_DIRS = getenv(
+		"XDG_DATA_DIRS", "/usr/local/share:/usr/share/")
+	XDG_CONFIG_DIRS = getenv("XDG_CONFIG_DIRS", "/etc/xdg")
+	XDG_CACHE_HOME = getenv("XDG_CACHE_HOME", home + "/.cache")
+)
+
+func getenv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) > 0 {
+		return value
+	}
+	return defaultValue
+}
---
+++ basedir/mkdirs.go
@@ -0,0 +1,20 @@
+package basedir
+
+import (
+	"os"
+	"path"
+)
+
+// make sure that the resulting path exists and mkdir missing dirs, if
+// necessary
+func makeEnvPathFunc(envVariable string) (func(string) error) {
+	return func(envPath string) error {
+		return os.MkdirAll(path.Join(envVariable, envPath), 0700)
+	}
+}
+
+var MakeDataPath = makeEnvPathFunc(XDG_DATA_HOME)
+
+var MakeConfigPath = makeEnvPathFunc(XDG_CONFIG_HOME)
+
+var MakeCachePath = makeEnvPathFunc(XDG_CACHE_HOME)
