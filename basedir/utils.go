b3fad685abbd8f560dc5351f1027d074e1de0b54
derdon <liedtke.simon@googlemail.com>
2012-03-01 23:16:49 +0100

    go fmt

--- basedir/utils.go
+++ basedir/utils.go
@@ -6,7 +6,7 @@ func (map1 environ) Eq(map2 environ) bool {
 	if !(len(map1) == len(map2)) {
 		return false
 	}
-	for key,value := range map1 {
+	for key, value := range map1 {
 		v, ok := map2[key]
 		if !ok || v != value {
 			return false
